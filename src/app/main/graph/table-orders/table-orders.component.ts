bc54dc4be711c4ca0bd264c235ed16d2cf6b1b56
erm.sergey <erm.sergey1989@gmail.com>
2018-08-24 16:18:18 +0300

    fixed v3

--- src/app/main/graph/table-orders/table-orders.component.ts
+++ src/app/main/graph/table-orders/table-orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-table-orders',
@@ -9,10 +9,21 @@ export class TableOrdersComponent implements OnInit {
 
   ordersTable1: any[];
   ordersTable2: any[];
+  tradesTable: any[];
+  tablesToggler = true;
+  enableToggler: boolean;
+
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.enableToggler = !!(window.innerWidth > 1023 && window.innerWidth < 1366);
+    console.log(this.enableToggler)
+  }
 
   constructor() { }
 
   ngOnInit() {
+
+    this.enableToggler = window.innerWidth > 1023 && window.innerWidth < 1366;
 
     this.ordersTable1 = [
         {marketSize: 211.570, price: 10.56, priceColor: 'green', mySize: '-'},
@@ -65,6 +76,58 @@ export class TableOrdersComponent implements OnInit {
           {marketSize: 211.570, price: 10.56, priceColor: 'red', mySize: '-'},
           {marketSize: 211.570, price: 10.56, priceColor: 'red', mySize: '-'},
       ];
+
+    this.tradesTable = [
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'down', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'down', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+      {tradeSize: 10.456, price: 10.57, priceDynamic: 'up', time: '1:12:16'},
+    ];
   }
 
 }
